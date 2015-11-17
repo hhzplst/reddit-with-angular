86589ad8523d272739a1ef7203cc720f50d69688
Ivy Chang <ivycwq@icloud.com>
2015-11-17 10:15:53 -0800

    fixing showform binding problem

--- js/controller.js
+++ js/controller.js
@@ -4,9 +4,15 @@ app.controller("postController", function($scope, $rootScope){
       url:"http://lorempixel.com/g/400/200/", 
       title:"My first post",
       content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+      url:"http://lorempixel.com/g/400/200/", 
+      title:"My first post",
+      content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
     }
       
   ];
+  $scope.view = {};
   $scope.addPost = function(){
     $scope.posts.push($scope.post);
   };
