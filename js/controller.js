24605defd33dae464b3ce5c647b4a789e73432a2
Ivy Chang <ivycwq@icloud.com>
2015-11-17 20:41:41 -0800

    fixing form validations and binding issues

--- js/controller.js
+++ js/controller.js
@@ -1,19 +1,24 @@
-app.controller("postController", function($scope, $rootScope){
+app.controller("PostController", function($scope, $rootScope){
   $scope.posts = [
     {
       url:"http://lorempixel.com/g/400/200/", 
       title:"My first post",
-      content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+      author: "Ivy",
+      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
     },
     {
       url:"http://lorempixel.com/g/400/200/", 
-      title:"My first post",
-      content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+      title:"My second post",
+      author:"Ivy",
+      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry."
     }
-      
   ];
   $scope.view = {};
-  $scope.addPost = function(){
-    $scope.posts.push($scope.post);
+  $scope.form = {};
+  $scope.addPost = function(newPost){
+    $scope.posts.push(newPost);
+    $scope.post = {};
+    $scope.form.newPost.$setPristine();
+    $scope.view.showform = false;
   };
 });
