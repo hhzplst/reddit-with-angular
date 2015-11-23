261f9f1a7e21a24e2821034b3ce32af517769594
Ivy Chang <ivycwq@icloud.com>
2015-11-22 18:27:43 -0800

    adding comments

--- js/controller.js
+++ js/controller.js
@@ -7,7 +7,9 @@ app.controller("PostController", function($scope, $rootScope){
       description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
       up: 0,
       down: 0,
-      date: "Sun Nov 15 2015 16:29:31 GMT-0800 (PST)"
+      date: "Sun Nov 15 2015 16:29:31 GMT-0800 (PST)",
+      comment: {show: false, text:""},
+      comments: ["nice!", "great!"]
     },
     {
       url:"http://lorempixel.com/g/400/200/", 
@@ -16,20 +18,32 @@ app.controller("PostController", function($scope, $rootScope){
       description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
       up: 0,
       down: 0,
-      date: "Tue Nov 17 2015 16:29:31 GMT-0800 (PST)"
+      date: "Tue Nov 17 2015 16:29:31 GMT-0800 (PST)",
+      comment: {show: false, text:""},
+      comments: ["nice!", "great!"]
     }
   ];
   $scope.view = {};
   $scope.form = {};
+  $scope.post = {};
+  $scope.post.comment = {};
   $scope.addPost = function(newPost){
-    //creating a date object
-
     newPost.up = 0;
     newPost.down = 0;
     newPost.date = new Date();
+    newPost.comment = {
+      show: false,
+      text: ""
+    };
+    newPost.comments = [];
     $scope.posts.push(newPost);
     $scope.post = {};
-    $scope.form.newPost.$setPristine();
+    $scope.form = {};
     $scope.view.showform = false;
+  };
+  $scope.addComment = function(newComment, index){
+    $scope.posts[index].comments.push(newComment.text);
+    $scope.posts[index].comment.text = "";
+    $scope.posts[index].comment.show = false;
   };
 });
